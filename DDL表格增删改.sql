24224f11ed137179ae760413bfb997fe6419f0b4
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-10-04 21:17:05 +0800

    Update DDL表格增删改.sql

--- DDL表格增删改.sql
+++ DDL表格增删改.sql
@@ -5,4 +5,5 @@ alter table z_use add nickname varchar(20) comment '昵称';
 alter table z_use change bian id int comment'编号';
 alter table z_use drop nickname;
 alter table z_use rename to emp;
+drop table if exists emp;
 desc emp;
