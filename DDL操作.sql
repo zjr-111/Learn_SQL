cd8195f769d97f14d050a3c161b8cd236d66cc9a
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-10-04 21:14:22 +0800

    Create DDL操作.sql

---
+++ DDL操作.sql
@@ -0,0 +1,8 @@
+use dddd;
+show tables;
+desc z_use;
+alter table z_use add nickname varchar(20) comment '昵称';
+alter table z_use change bian id int comment'编号';
+alter table z_use drop nickname;
+alter table z_use rename to emp;
+desc emp;
