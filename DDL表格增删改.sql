e715727161810bc17d303db466140d8cf6e86364
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-10-05 20:43:58 +0800

    Update DDL表格增删改.sql

--- DDL表格增删改.sql
+++ DDL表格增删改.sql
@@ -1,9 +1,15 @@
 use dddd;
 show tables;
 desc z_use;
+# z_use表格增加名为nickname的行
 alter table z_use add nickname varchar(20) comment '昵称';
+# 更改表格内标签的名称
 alter table z_use change bian id int comment'编号';
+# 删除z_use表格里的nickname行
 alter table z_use drop nickname;
+# 将z_use表格改名为emp
 alter table z_use rename to emp;
+# 如果emp表格存在删除表格
 drop table if exists emp;
+# 查看表格
 desc emp;
